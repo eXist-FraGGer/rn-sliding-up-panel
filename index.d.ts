dd2cd5110287970c26a7f8fe975b92eb14233dfb
denis-shostik <36534894+denis-shostik@users.noreply.github.com>
2018-11-19 00:36:15 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,29 @@
 declare module 'rn-sliding-up-panel' {
-  import * as React               from 'react';
-  import { StyleProp, ViewStyle } from 'react-native';
+    import * as React               from 'react';
+    import { StyleProp, ViewStyle } from 'react-native';
 
-  interface SlidingUpPanelProps {
-    visible?: boolean,
-    draggableRange?: {
-      top: number,
-      bottom: number
-    },
-    height?: number,
-    renderSlidingControl?: Function,
-    onDrag?: Function,
-    onDragStart?: Function,
-    onDragEnd?: Function,
-    onRequestClose?: Function,
-    allowMomentum?: boolean,
-    allowDragging?: boolean,
-    showBackdrop?: boolean,
-    contentStyle?: StyleProp<ViewStyle>,
-    containerStyle?: StyleProp<ViewStyle>,
-    swipeControlColor?: string,
-    swipeControlHeight?: number
-  }
+    interface SlidingUpPanelProps {
+        visible?: boolean,
+        draggableRange?: {
+            top: number,
+            bottom: number
+        },
+        height?: number,
+        renderSlidingControl?: Function,
+        onDrag?: Function,
+        onDragStart?: Function,
+        onDragEnd?: Function,
+        onRequestClose?: Function,
+        allowMomentum?: boolean,
+        allowDragging?: boolean,
+        showBackdrop?: boolean,
+        contentStyle?: StyleProp<ViewStyle>,
+        containerStyle?: StyleProp<ViewStyle>,
+        swipeControlColor?: string,
+        swipeControlHeight?: number
+    }
 
-  class SlidingUpPanel extends React.Component<SlidingUpPanelProps, any> {}
+    class SlidingUpPanel extends React.Component<SlidingUpPanelProps, any> {}
 
-  export default SlidingUpPanel;
+    export default SlidingUpPanel;
 }
